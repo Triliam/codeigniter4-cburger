306144a4674e99dea03bb6793b079200895d96c1
Triliam <taisbuenovidotto@gmail.com>
2025-07-02 13:45:11 -0300

    adicionando filtro de verificação se o usuário está logado

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -2,6 +2,7 @@
 
 namespace Config;
 
+use App\Filters\UserIsLoggedIn;
 use CodeIgniter\Config\Filters as BaseFilters;
 use CodeIgniter\Filters\Cors;
 use CodeIgniter\Filters\CSRF;
@@ -34,6 +35,7 @@ class Filters extends BaseFilters
         'forcehttps'    => ForceHTTPS::class,
         'pagecache'     => PageCache::class,
         'performance'   => PerformanceMetrics::class,
+        'userisloggedin'=> UserIsLoggedIn::class,
     ];
 
     /**
@@ -69,6 +71,7 @@ class Filters extends BaseFilters
      */
     public array $globals = [
         'before' => [
+            'userisloggedin' => ['except' => ['auth/login', 'auth/submit']]
             // 'honeypot',
             // 'csrf',
             // 'invalidchars',
